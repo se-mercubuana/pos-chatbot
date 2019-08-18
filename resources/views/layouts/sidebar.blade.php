0a9876e5b1e94d310d3e1a1d3b4a1136d901f9a7
Ramdani024 <faniramdani.24@gmail.com>
2019-08-18 20:39:14 +0700

    Menu Logout dan Icon sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -6,28 +6,28 @@
             <ul id="sidebarnav" class="p-t-30">
                 <li class="sidebar-item"><a class="sidebar-link waves-effect waves-dark sidebar-link"
                                             href="/home" aria-expanded="false"><i
-                                class="mdi mdi-view-dashboard"></i><span class="hide-menu">Dashboard</span></a></li>
+                                class="mdi mdi-home"></i><span class="hide-menu">Dashboard</span></a></li>
 
                 <li class="sidebar-item"><a class="sidebar-link waves-effect waves-dark sidebar-link"
                                             href="/bank" aria-expanded="false"><i
-                                class="mdi mdi-view-dashboard"></i><span class="hide-menu">Bank</span></a></li>
+                                class="mdi mdi-bank"></i><span class="hide-menu">Bank</span></a></li>
 
                 <li class="sidebar-item"><a class="sidebar-link waves-effect waves-dark sidebar-link"
                                             href="/user" aria-expanded="false"><i
-                                class="mdi mdi-view-dashboard"></i><span class="hide-menu">User</span></a></li>
+                                class="mdi mdi-account"></i><span class="hide-menu">User</span></a></li>
 
                 <li class="sidebar-item"><a class="sidebar-link waves-effect waves-dark sidebar-link"
                                             href="/customer" aria-expanded="false"><i
-                                class="mdi mdi-view-dashboard"></i><span class="hide-menu">Customer</span></a></li>
+                                class="mdi mdi-account-multiple"></i><span class="hide-menu">Customer</span></a></li>
 
                 <li class="sidebar-item"><a class="sidebar-link waves-effect waves-dark sidebar-link"
                                             href="/product" aria-expanded="false"><i
-                                class="mdi mdi-view-dashboard"></i><span class="hide-menu">Produk</span></a></li>
+                                class="mdi mdi-shopping"></i><span class="hide-menu">Produk</span></a></li>
 
 
                 <li class="sidebar-item"><a class="sidebar-link has-arrow waves-effect waves-dark"
                                             href="javascript:void(0)" aria-expanded="false"><i
-                                class="mdi mdi-receipt"></i><span class="hide-menu">Transaksi </span></a>
+                                class="mdi mdi-cart-outline"></i><span class="hide-menu">Transaksi </span></a>
                     <ul aria-expanded="false" class="collapse  first-level">
                         <li class="sidebar-item"><a href="/transaction" class="sidebar-link"><i
                                         class="mdi mdi-note-outline"></i><span
@@ -45,6 +45,9 @@
                     </ul>
                 </li>
 
+                <li class="sidebar-item"><a class="sidebar-link waves-effect waves-dark sidebar-link"
+                                            href="/logout" aria-expanded="false"><i
+                                class="mdi mdi-logout-variant"></i><span class="hide-menu">Logout</span></a></li>
             </ul>
         </nav>
         <!-- End Sidebar navigation -->
